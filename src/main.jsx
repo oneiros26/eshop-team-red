5e20d33595b46c27d4893dfd5ae703df829d8a26
Jakub Zlámal <kub1no23@100HPJakoubek.>
2025-05-26 13:24:40 +0200

    add cartContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import DataProvider from './context/DataProvider.jsx';
+import CartProvider from './context/CartProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <DataProvider>
-            <App />
+            <CartProvider>
+                <App />
+            </CartProvider>
         </DataProvider>
     </StrictMode>,
 )
