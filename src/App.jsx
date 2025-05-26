94a327affd37e94dabfd7537a9326c8f9ef79a40
Antonín Studeník <tonik.studenik0@gmail.com>
2025-05-26 13:47:35 +0200

    added folders and files to add image, first attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import HeroSection from './components/features/hero-section/HeroSection';
+
 function App() {
-  return <h1 className="text-3xl font-bold bg-red-800">Team Red</h1>;
+  return(<>
+    <h1 className="text-3xl font-bold bg-red-800">Team Red</h1>
+    <HeroSection/>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/features/hero-section/HeroSection.jsx
@@ -0,0 +1,8 @@
+import './heroSection.css';
+import a from '../../../assets/a.webp';
+
+function HeroSection() {
+  return <img className="hero-image" src={a} alt="Hero" />;
+}
+
+export default HeroSection;
