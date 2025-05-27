95357f5f63f5ec969167e720c4f4bca930412d04
Vít Sobíšek <sobisekvit@gmail.com>
2025-05-27 13:27:48 +0200

    Add testimonials

---
+++ src/components/common/Testimonials.jsx
@@ -0,0 +1,36 @@
+function Testimonials() {
+  return (
+    <article className="flex w-full h-40 items-center justify-evenly bg-blue-100">
+      <section className="flex flex-col gap-1 items-center w-70 text-center">
+        <p className="text-blue-500 text-3xl">
+          &#9733;&#9733;&#9733;&#9733;&#9734;
+        </p>
+        <p className="text-md">"Drak better ngl."</p>
+        <h4 className="font-semibold text-lg">Honza Sníh</h4>
+      </section>
+      <section className="flex flex-col gap-1 items-center w-70 text-center">
+        <p className="text-blue-500 text-3xl">
+          &#9733;&#9733;&#9733;&#9733;&#9733;
+        </p>
+        <p className="text-md">"Haf haf."</p>
+        <h4 className="font-semibold text-lg">Theo Šedoslavný</h4>
+      </section>
+      <section className="flex flex-col gap-1 items-center w-70 text-center">
+        <p className="text-blue-500 text-3xl">
+          &#9733;&#9733;&#9733;&#9734;&#9734;
+        </p>
+        <p className="text-md">"Moje sestra je lepší."</p>
+        <h4 className="font-semibold text-lg">Marmeláda Lanová</h4>
+      </section>
+      <section className="flex flex-col gap-1 items-center w-70 text-center">
+        <p className="text-blue-500 text-3xl">
+          &#9733;&#9733;&#9734;&#9734;&#9734;
+        </p>
+        <p className="text-md">"Běžte se raději pomodlit!"</p>
+        <h4 className="font-semibold text-lg">Vysoký Vrabec</h4>
+      </section>
+    </article>
+  );
+}
+
+export default Testimonials;
