5b78a90c60dd87707a3823741ce7c806a4aa0b50
Vít Sobíšek <sobisekvit@gmail.com>
2025-05-27 13:48:13 +0200

    code cleanup

---
+++ src/components/features/HeroSection.jsx
@@ -0,0 +1,30 @@
+import img from "../../../assets/heroImige.webp";
+
+function HeroSection() {
+  return (
+    <div className="relative w-full h-screen">
+      <img src={img} alt="Pozadí" className="w-full h-full object-cover" />
+
+      <div className="absolute top-0 left-0 w-1/2 h-full flex items-center justify-start">
+        <div className="bg-black/60 text-white p-5 rounded-xl absolute top-12 left-5">
+          <h1 className="leading-none text-[clamp(2rem,5vw,3rem)] font-extrabold">
+            Vítejte ve světě rychlosti a dokonalého designu
+          </h1>
+          <p className="text-[clamp(1rem,3vw,1.5rem)] mt-4">
+            Neprodáváme jen auta – nabízíme životní styl. Špičkové sportovní
+            vozy a formulové speciály od prestižních značek. Výkon, technologie
+            a krása v dokonalé harmonii.
+            <br />
+            Pro závodní adrenalin i elegantní dominanci na silnici. Vaše vášeň
+            začíná zde.
+          </p>
+          <button className="overlay-button whitespace-nowrap absolute -bottom-20 left bg-blue-700/80 hover:bg-blue-600 text-white py-3 px-6 text-lg rounded-lg hover:opacity-100 transition-opacity duration-300">
+            Přejdi na trending produkty.
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
--- src/components/features/TrendingSection.jsx
+++ src/components/features/TrendingSection.jsx
@@ -1,43 +1,55 @@
 import React, { useContext } from "react";
-import ProductCard from "./product-card/ProductCard";
+import ProductCard from "./ProductCard";
 import { DataContext } from "../../context/DataProvider";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/autoplay';
-import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper/modules";
 
 function TrendingSection() {
-    const apiData = useContext(DataContext)
+  const apiData = useContext(DataContext);
 
-    const selectedIds = [8, 6, 9, 12, 7, 2]
+  const selectedIds = [8, 6, 9, 12, 7, 2];
 
-    const trendingProducts = apiData.filter(product =>
-        selectedIds.includes(product.id)
-    )
+  const trendingProducts = apiData.filter((product) =>
+    selectedIds.includes(product.id)
+  );
 
-    return(
-        <div>
-            <h2>Trending</h2>
-            <Swiper 
-                spaceBetween={20}
-                slidesPerView={'5'}
-                autoplay={{ delay: 1000, disableOnInteraction: false, pauseOnMouseEnter: true }}
-                loop={true}
-                modules={[Autoplay]}
-                style={{backgroundColor: "green"}}
-            >
-                {trendingProducts.map((product) => (
-                    <SwiperSlide style={{ width: '220px', backgroundColor: "yellow", display: "flex", justifyContent: "center"}} key={product.id}>
-                        <ProductCard
-                            image={product.image}
-                            title={product.title}
-                            price={product.price}
-                        />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Trending</h2>
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={"5"}
+        autoplay={{
+          delay: 1000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        loop={true}
+        modules={[Autoplay]}
+        style={{ backgroundColor: "green" }}
+      >
+        {trendingProducts.map((product) => (
+          <SwiperSlide
+            style={{
+              width: "220px",
+              backgroundColor: "yellow",
+              display: "flex",
+              justifyContent: "center",
+            }}
+            key={product.id}
+          >
+            <ProductCard
+              image={product.image}
+              title={product.title}
+              price={product.price}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
 
-export default TrendingSection
+export default TrendingSection;
