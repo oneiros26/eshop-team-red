7ab3b85d01edc1af0f868ba88f003935a486240e
Vít Sobíšek <sobisekvit@gmail.com>
2025-05-27 09:06:15 +0200

    Added product page and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,56 @@
 import React, { useContext } from "react";
 import ProductCard from "./components/features/product-card/ProductCard";
 import { DataContext } from "./context/DataProvider";
+import ProductPage from "./pages/ProductPage";
+import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
 
 function App() {
   const apiData = useContext(DataContext);
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <main>
+          <h1 className="text-3xl font-bold bg-red-800">Team Red</h1>
+          <div className="flex flex-wrap">
+            {apiData.map((product) => (
+              <ProductCard
+                key={product.id}
+                image={product.image}
+                title={product.title}
+                price={product.price}
+              />
+            ))}
+          </div>
+        </main>
+      ),
+    },
+    {
+      path: "/product/:productId",
+      element: <ProductPage />,
+    },
+    {
+      path: "/cart",
+      element: <p>CART COMPONENT</p>,
+    },
+    {
+      path: "/checkout",
+      element: <p>CHECKOUT COMPONENT</p>,
+    },
+    {
+      path: "/checkout-confirm",
+      element: <p>CONFIRM COMPONENT</p>,
+    },
+    {
+      path: "/all-products",
+      element: <p>CHECKOUT COMPONENT</p>,
+    },
+  ]);
+
   return (
     <>
-      <h1 className="text-3xl font-bold bg-red-800">Team Red</h1>
-      <div className="flex flex-wrap">
-        {apiData.map((product) => (
-          <ProductCard
-            key={product.id}
-            image={product.image}
-            title={product.title}
-            price={product.price}
-          />
-        ))}
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
