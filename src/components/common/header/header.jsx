1bc5c6f01b27fdc52af5a95f63d65b18c3607a97
Jakub Zlámal <kub1no23@100HPJakoubek.>
2025-05-28 14:14:15 +0200

    add sideCart styles and fix behaviour

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -1,57 +1,54 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import cartIconClosed from "./../../../assets/icons/cartIconClosed.png";
+import cartIconClosed from "./../../../assets/icons/cartIconClosed.svg";
 import cartIconOpen from "./../../../assets/icons/cartIconOpen.png";
 import CartSidebar from "./CartSidebar";
 
 function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [showCart, setShowCart] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [showCart, setShowCart] = useState(false);
 
-  function toggleCart() {
-    setShowCart((c) => !c);
-  }
-
-  return (
-    <header className="fixed top-0 left-0 w-full bg-white shadow-md z-50 h-16 flex items-center px-6 justify-between">
-      <Link to="/">
-        <img
-          className="h-10 w-auto object-contain"
-          src="https://fullsend.agency/wp-content/uploads/2024/10/ferrari-logo.jpg"
-          alt="logo"
-        />
-      </Link>
-      <section></section>
-      <nav>
-        <ul className="flex items-center gap-6">
-          <li>
-            <Link to="/" className="hover:underline">
-              Domov
+    return (
+        <header className="fixed top-0 left-0 w-full bg-white shadow-md z-20 h-16 flex items-center px-6 justify-between">
+            <Link to="/">
+                <img
+                    className="h-10 w-auto object-contain"
+                    src="https://fullsend.agency/wp-content/uploads/2024/10/ferrari-logo.jpg"
+                    alt="logo"
+                />
             </Link>
-          </li>
-          <li>
-            <Link to="/all-products" className="hover:underline">
-              Produkty
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" className="hover:underline">
-              Kontakty
-            </Link>
-          </li>
-          <li>
-            <img
-              src={showCart ? cartIconOpen : cartIconClosed}
-              alt="shopping cart icon"
-              onClick={toggleCart}
-              className="h-6 w-6 cursor-pointer"
-            />
-          </li>
-          {showCart && <CartSidebar></CartSidebar>}
-        </ul>
-      </nav>
-    </header>
-  );
+            <section></section>
+            <nav className="h-full">
+                <ul onMouseLeave={() => setShowCart(false)} className="flex items-center gap-6 h-full">
+                    <li>
+                        <Link to="/" className="hover:underline">
+                            Domov
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/all-products" className="hover:underline">
+                            Produkty
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contact" className="hover:underline">
+                            Kontakty
+                        </Link>
+                    </li>
+                    <li>
+                        <img
+                            src={showCart ? cartIconOpen : cartIconClosed}
+                            alt="shopping cart icon"
+                            className="h-6 w-6 cursor-pointer"
+                            onMouseEnter={() => setShowCart(true)}
+                            onClick={() => setShowCart(!showCart)}
+                        />
+                        {showCart && <CartSidebar></CartSidebar>}
+                    </li>
+                </ul>
+            </nav>
+        </header >
+    );
 }
 
 export default Header;
