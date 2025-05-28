b20df3dc47e1a996a67fdbd504f67df1fd0171a7
JohnDoli <JohnDoli@users.noreply.github.com>
2025-05-28 10:07:42 +0200

    add product card redirection to all products page - without applying a filter

--- src/components/features/CategoryCard.jsx
+++ src/components/features/CategoryCard.jsx
@@ -1,19 +1,20 @@
+import { Link } from "react-router-dom";
+
+
 
 function ProductCard({title="not defined", image="https://placehold.co/450x300"}) {
-
-    function handleOnClick() {
-        console.log("category clicked!")
-    }
-
+    
     return (
-        <div onClick={handleOnClick} className="flex-shrink-0 bg-cover h-[300px] w-[450px] relative">
-            <img src={image} className="h-[300px] w-[450px] rounded-[40px]"/>
-            <div className="w-fit h-fit">
-                <h3 className="text-white text-center align-middle text-[32px] font-bold absolute left-8 bottom-8 text-shadow-lg backdrop-blur-md p-[4px] rounded-2xl">
-                    {title}
-                </h3>
+        <Link to={`/all-products`} className="flex-shrink-0">
+            <div className="flex-shrink-0 bg-cover h-[300px] w-[450px] relative">
+                <img src={image} className="h-[300px] w-[450px] rounded-[40px]"/>
+                <div className="w-fit h-fit">
+                    <h3 className="text-white text-center align-middle text-[32px] font-bold absolute left-8 bottom-8 text-shadow-lg backdrop-blur-md p-[4px] rounded-2xl">
+                        {title}
+                    </h3>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
