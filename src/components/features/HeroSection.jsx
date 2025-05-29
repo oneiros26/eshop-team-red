8890d326e345e0d93d7878a83e1ceee797d660dd
JohnDoli <JohnDoli@users.noreply.github.com>
2025-05-29 13:30:24 +0200

    gradient to main image

--- src/components/features/HeroSection.jsx
+++ src/components/features/HeroSection.jsx
@@ -3,9 +3,11 @@ import img from "../../assets/SF-24-Launch-GIF-2_2048x2048-xd.webp";
 function HeroSection() {
   return (
     <div className="relative w-full h-screen mt-16">
-      <img src={img} alt="Pozadí" className="w-full h-full object-cover" />
+      <div className="bg-gradient-to-br from-black to-transparent w-full h-full absolute">
+          <img src={img} alt="Pozadí" className="w-full h-full object-cover relative -z-10"/>
+      </div>
       <div className="absolute top-0 left-0 w-1/2 h-full flex items-center justify-start">
-        <div className="bg-black/60 text-white p-5 rounded-xl absolute top-12 left-5">
+        <div className="text-white p-5 rounded-xl absolute top-12 left-5">
           <h1 className="leading-none text-[clamp(2rem,5vw,3rem)] font-extrabold">
             Vítejte ve světě rychlosti a dokonalého designu
           </h1>
