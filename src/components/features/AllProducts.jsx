73dc371e78ca86dcb1665ec6eec746011f5a2561
JohnDoli <JohnDoli@users.noreply.github.com>
2025-05-29 11:17:01 +0200

    update - filtering based on category

--- src/components/features/AllProducts.jsx
+++ src/components/features/AllProducts.jsx
@@ -8,6 +8,7 @@ function AllProducts() {
     const apiData = useContext(DataContext)
 
     const [selectedIds, setSelectedIds] = useState([]);
+    const [category, setCategory] = useState("all");
 
     useEffect(() => {
         if (apiData && apiData.length > 0) {
@@ -15,11 +16,13 @@ function AllProducts() {
         }
     }, [apiData]);
 
-    const selectedProducts = apiData.filter((product) =>
-        selectedIds.includes(product.id)
-    );
+    const selectedProducts = apiData
+        .filter((product) => selectedIds.includes(product.id))
+        .filter((product) =>
+            category === "all" ? true : product.category === category // pokud category = all -> select vsechny produkty
+        );
 
-    function handleChange(e) {
+    function handleIdChange(e) {
         if (e.target.checked === true) {
             setSelectedIds([8, 6, 9])
         }
@@ -28,10 +31,22 @@ function AllProducts() {
         }
     }
 
+    function handleCategoryChange(e) {
+        setCategory(e.target.value);
+    }
+
     return(
-        <div>
-            <ProductFilter onChange={handleChange}/>
-            <div className="mt-[64px] flex flex-col text-center items-center justify-center">
+        <div className="flex flex-row">
+            <div className="w-1/10 min-w-[200px] flex flex-col items-start justify-start mt-[64px] mr-8">
+                <ProductFilter onChange={handleIdChange} name={"id"}/>
+                <p>CATEGORIES</p>
+                <ProductFilter onChange={handleCategoryChange} name={"all"} checked={category === "all"} value={"all"}/>
+                <ProductFilter onChange={handleCategoryChange} name={"electronics"} checked={category === "electronics"} value={"electronics"}/>
+                <ProductFilter onChange={handleCategoryChange} name={"jewelery"} checked={category === "jewelery"} value={"jewelery"}/>
+                <ProductFilter onChange={handleCategoryChange} name={"men's clothing"} checked={category === "men's clothing"} value={"men's clothing"}/>
+                <ProductFilter onChange={handleCategoryChange} name={"women's clothing"} checked={category === "women's clothing"} value={"women's clothing"}/>
+            </div>
+            <div className="mt-[64px] flex flex-col text-center items-center justify-center w-4/5">
                 <h1 className="m-[40px] font-bold text-5xl text-gray-800">
                     all products
                 </h1>
--- src/components/features/ProductFilter.jsx
+++ src/components/features/ProductFilter.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 
-function ProductFilter({ onChange, name }) {
+function ProductFilter({ onChange, name, checked, value }) {
 
-    return(
-        <div className="mt-[64px] flex flex-col border-green-800 border-3 pl-30 pr-30">
-            <label className="text-gray-800 font-bold text-lg ml-2">{name}</label>
-            <div className="flex flex-row border-3">
-                <p>3</p>
-                <input type="checkbox" onChange={onChange}/>
+    return (
+        <div className="m-2 w-full">
+            <div className="flex flex-row justify-between">
+                <label className="text-gray-700 font-medium" htmlFor={name}>
+                    {name}
+                </label>
+                <input
+                    id={name}
+                    type="checkbox"
+                    value={value}
+                    checked={checked}
+                    onChange={onChange}
+                    className="h-5 w-5 border-gray-300 rounded"
+                />
             </div>
         </div>
     )
