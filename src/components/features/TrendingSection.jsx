fe4382993f068b0acc298690c5ad7df18b736af1
JohnDoli <JohnDoli@users.noreply.github.com>
2025-05-29 12:24:21 +0200

    add ids by index

--- src/components/features/TrendingSection.jsx
+++ src/components/features/TrendingSection.jsx
@@ -9,7 +9,11 @@ import { Autoplay } from "swiper/modules";
 function TrendingSection() {
   const apiData = useContext(DataContext);
 
-  const selectedIds = [8, 6, 9, 12, 7, 2];
+  console.log("ids", apiData.map((product) => product.id));
+
+  const ids = apiData.map((product) => product.id)
+
+  const selectedIds = [ids[0], ids[1], ids[2], ids[3], ids[4], ids[5], ids[6], ids[7]];
 
   const trendingProducts = apiData.filter((product) =>
     selectedIds.includes(product.id)
@@ -19,8 +23,8 @@ function TrendingSection() {
     <div className="my-10">
       <h2 className="text-center font-semibold text-5xl p-8">Trending</h2>
       <Swiper
-        spaceBetween={20}
-        slidesPerView={"5"}
+        spaceBetween={40}
+        slidesPerView={5}
         autoplay={{
           delay: 1500,
           disableOnInteraction: false,
@@ -30,12 +34,31 @@ function TrendingSection() {
         modules={[Autoplay]}
         style={{ backgroundColor: "white" }}
         speed={2000}
+        grabCursor={true}
+        breakpoints={
+          {
+            320: {
+              slidesPerView: 1,
+            },
+            640: {
+              slidesPerView: 2,
+            },
+            768: {
+              slidesPerView: 3,
+            },
+            1024: {
+              slidesPerView: 4,
+            },
+            1280: {
+              slidesPerView: 5,
+            },
+          }
+        }
       >
         {trendingProducts.map((product) => (
           <SwiperSlide
             style={{
-              width: "220px",
-              backgroundColor: "#dbeafe",
+              width: "fit-content",
               display: "flex",
               justifyContent: "center",
               padding: "1rem",
